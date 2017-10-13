5c4567e01748aeaec90a9a36adbd685bc7cb826b
Marcin <marcinhesse@gmail.com>
2017-10-13 14:41:35 +0200

    utworzenie drugiej aktywności i wywołanie jej przyciskiem (bez przekazania danych)

---
+++ app/src/main/java/com/a3torstudio/createmessageactivity/ReciveMessageActivity.java
@@ -0,0 +1,13 @@
+package com.a3torstudio.createmessageactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReciveMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recive_message);
+    }
+}
