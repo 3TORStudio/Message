dec2e01f5041636fe17a5090a9f3583c75c4f6de
Marcin <marcinhesse@gmail.com>
2017-10-13 22:35:02 +0200

    dokończenie programu z intencjami jawnymi

--- app/src/main/java/com/a3torstudio/createmessageactivity/CreateMessage.java
+++ app/src/main/java/com/a3torstudio/createmessageactivity/CreateMessage.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class CreateMessage extends AppCompatActivity {
 
@@ -14,7 +15,10 @@ public class CreateMessage extends AppCompatActivity {
     }
 
     public void onSendMessage(View view){
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
         Intent intent = new Intent(this,ReciveMessageActivity.class);
+        intent.putExtra(ReciveMessageActivity.EXTRA_MESSAGE,messageText);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/a3torstudio/createmessageactivity/ReciveMessageActivity.java
+++ app/src/main/java/com/a3torstudio/createmessageactivity/ReciveMessageActivity.java
@@ -1,13 +1,20 @@
 package com.a3torstudio.createmessageactivity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReciveMessageActivity extends AppCompatActivity {
 
+    public static final String EXTRA_MESSAGE = "message";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recive_message);
+        Intent intent = getIntent();
+        String messageText = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView messageView = (TextView) findViewById(R.id.message);
+        messageView.setText(messageText);
     }
 }
